c7215c46f4a238bc74e612ddddc98d880135bb1c
pavitra297 <pavitrajasti3@gmail.com>
2025-04-07 21:01:36 +0530

    basic page created containing 6 cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import BrainParticles from './components/Background'
+import WhyParticipate from './components/WhyParticipate'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <BrainParticles />
+     {/* <BrainParticles /> */}
+     <WhyParticipate/>
     </>
   )
 }
