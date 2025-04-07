025449d78f9c1f8178170aed9fbd64458b02b36d
vaishu280606 <jvaishnavi2806@gmail.com>
2025-04-07 23:16:12 +0530

    First rewards commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import BrainParticles from './components/Background'
+import Rewards from './components/Rewards';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,9 @@ function App() {
   return (
     <>
      <BrainParticles />
+     <div className="content-wrapper">
+      <Rewards/>
+     </div>
     </>
   )
 }
